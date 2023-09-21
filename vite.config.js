0a91eb85bfa21bf8965b92d2cf41087319302738
tint <devtint@gmail.com>
2023-09-21 14:13:32 +0800

    修改本地开发端口

--- vite.config.js
+++ vite.config.js
@@ -43,7 +43,7 @@ export default defineConfig(({ mode }) => {
     base: viteEnv.VITE_BASE,
     server: {
       host: '0.0.0.0',
-      port: '8080',
+      port: '7749',
       open: true,
       // 端口占用直接退出
       strictPort: true
