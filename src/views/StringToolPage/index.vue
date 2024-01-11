da6cb3954c5be1d812e64d96ed8f2c48023f59ac
wloop <devtint@gmail.com>
2024-01-11 17:19:01 +0800

    add: 帕斯卡命名法转換

--- src/views/StringToolPage/index.vue
+++ src/views/StringToolPage/index.vue
@@ -127,19 +127,19 @@ const convert = value => {
       inputChange(result.value)
       break
     case 'pascal_case':
-      // 帕斯卡转換
+      // 帕斯卡命名法转換
+      result.value = inputText.value
+        .replace(/[-_\s]+(.)?/g, function (match, c) {
+          return c ? c.toUpperCase() : ''
+        })
+        .replace(/^[a-z]/, function (match) {
+          return match.toUpperCase()
+        })
+      inputChange(result.value)
       break
 
     default:
       break
-  }
-
-  function toHumpName(str) {
-    var re = /-(\w)/g // \w匹配所有的字母和数字字符以及下划线_
-    return str.replace(re, function ($0, $1) {
-      // 第一个参数re表示正则,第二个回调函数,对该字符串处理方式,圆括号内的形参数表示正则表达式的子选项
-      return $1.toUpperCase() // 转大写
-    })
   }
 }
 </script>
